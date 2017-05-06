b5fd8def53e2574eee5831de6f0f975ec0a12ad9
kewalaka <kewalaka@gmail.com>
2017-05-07 09:30:27 +1200

    add FME Desktop

---
+++ fmedesktop/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName = 'fmedesktop'
+$url32       = 'https://downloads.safe.com/fme/2017/fme-desktop-2017.0-b17288-win-x86.msi'
+$url64       = 'https://downloads.safe.com/fme/2017/fme-desktop-2017.0-b17288-win-x64.msi'
+$checksum32  = ''
+$checksum64  = ''
+ 
+$packageArgs = @{
+  packageName            = $packageName
+  fileType               = 'msi'
+  url                    = $url32
+  url64bit               = $url64
+  checksum               = $checksum32
+  checksum64             = $checksum64
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/qn INSTALLLEVEL=3 /log `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).install.log`"'
+  validExitCodes         = @(0)
+  registryUninstallerKey = 'FME Desktop'
+}
+Install-ChocolateyPackage @packageArgs
+ 
+$installLocation = Get-AppInstallLocation $packageArgs.registryUninstallerKey
+if ($installLocation)  {
+    Write-Host "$packageName installed to '$installLocation'"
+}
+else { Write-Warning "Can't find $PackageName install location" }
+
