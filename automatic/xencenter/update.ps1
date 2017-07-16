dc4992195a4a4bd358418b71ec34b31b4e24e0ff
Stu Mace <kewalaka@gmail.com>
2017-07-16 15:23:12 +1200

    correct comments

--- automatic/xencenter/update.ps1
+++ automatic/xencenter/update.ps1
@@ -11,13 +11,15 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-    # getting the latest version via bintray
+    # getting the latest version from Citrix RSS feed
     $rssfeed = Invoke-WebRequest 'https://www.citrix.com/content/citrix/en_us/downloads/xenserver.rss'
+    # .. we want the URL for the last available "standard edition" version
     $base_uri = (([xml]$rssfeed.content).rss.channel.item | Where-Object {$_.title -like '*Standard Edition*'} | Select-Object -first 1).link
     $downloadpage = Invoke-WebRequest $base_uri
 
     if ($downloadpage.StatusCode -eq 200)
     {
+        # example URL: http://downloadns.citrix.com.edgesuite.net/12637/XenServer-7.2.0-XenCenterSetup.exe
         $url = ($downloadpage.AllElements | Where-Object rel -like '*XenCenterSetup.exe').rel
         $version = ($url.split('/') | Select-Object -last 1).split('-')[1]
 
