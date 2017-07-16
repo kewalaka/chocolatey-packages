4c968f04af358975b80fccd939963bc9f0384dd6
Stu Mace <kewalaka@gmail.com>
2017-07-16 14:47:12 +1200

    xencenter AU updates

--- automatic/xencenter/tools/chocolateyinstall.ps1
+++ automatic/xencenter/tools/chocolateyinstall.ps1
@@ -1,16 +1,26 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
 
-$packageName= 'xencenter'
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
-  packageName            = $packageName
-  fileType               = 'EXE'
+  packageName            = 'xencenter'
+  fileType               = 'exe'
+  url                    = ''
   url64bit               = 'http://downloadns.citrix.com.edgesuite.net/12637/XenServer-7.2.0-XenCenterSetup.exe'
+  checksum               = ''
   checksum64             = 'EED2AD46C7E89D6880DB83E4599864AF9C46607B8EFB760FF583A70332CAD63F'
+  checksumType           = 'sha256'
   checksumType64         = 'sha256'
   silentArgs             = "/i /q /Log /LogFile `"$env:TEMP\chocolatey\$($packageName)\$($packageName).Install.log`""
   validExitCodes         = @(0, 3010, 1641)
-  softwareName           = 'Citrix XenCenter'
+  softwareName           = 'xencenter*'
 }
-
 Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
--- automatic/xencenter/update.ps1
+++ automatic/xencenter/update.ps1
@@ -3,12 +3,8 @@ import-module au
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
             "(?i)(^\s*url64bit\s*=\s*)('.*')"     = "`$1'$($Latest.URL64)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
-            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
-            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
         }
     }
 }
@@ -22,10 +18,13 @@ function global:au_GetLatest {
 
     if ($downloadpage.StatusCode -eq 200)
     {
-        $url64 = ($downloadpage.AllElements | where rel -like '*XenCenterSetup.exe').rel
+        $url64 = ($downloadpage.AllElements | Where-Object rel -like '*XenCenterSetup.exe').rel
         $version = ($url64.split('/') | Select-Object -last 1).split('-')[1]
 
-        @{ URL64=$url64; Version = $version }
+        @{ 
+            URL64   = $url64;
+            Version = $version;
+        }
     }
 
 }
